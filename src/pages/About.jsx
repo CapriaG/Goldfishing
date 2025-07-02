ee31120fade4bb66a6d3ebdec3addde60e8341dc
Gabriel Capria <105518132+CapriaG@users.noreply.github.com>
2025-07-01 23:17:08 -0300

    change about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
       <div className="about-card text-center p-4">
         <h2 className="about-modern-title">Sobre Goldfishin</h2>
         <p className="about-modern-text">
-          En Goldfishin nos apasiona la pesca y la caza. Ofrecemos productos seleccionados que combinan calidad y rendimiento para acompañarte en cada aventura.
+          Nuestra empresa se caracteriza por comercializar artículos de camping, pesca y cuchillería. Con 35 años de experiencia ayudando y orientando a nuestros clientes en sus compras según la zona que corresponda.
           <br />
-          Nuestra misión es brindar herramientas confiables y duraderas, pensadas para quienes disfrutan del contacto con la naturaleza y buscan siempre ir un paso más allá. Desde cañas, anzuelos y accesorios técnicos, hasta indumentaria resistente y equipamiento para caza, trabajamos con marcas reconocidas y materiales de primera calidad.
+          Contamos con muchos artículos tanto importados como de industria nacional, incluyendo sus repuestos. Estamos asesorados por personas expertas amantes de la pesca deportiva y la vida al aire libre.
         </p>
         <a
           href="https://wa.me/5491130842824" // Reemplazá con tu número real
